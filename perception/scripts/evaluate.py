43ff16f4d0691826b5168e2d89b592d0591a8e65
matteias <mattias.hansson@live.se>
2022-03-25 14:42:44 +0100

    Added calibrationg and tests with grabcut

--- perception/scripts/evaluate.py
+++ perception/scripts/evaluate.py
@@ -166,9 +166,11 @@ class network():
         y_start = max(pad,min(h-pad,y_start))
         y_end = max(pad,min(h-pad,y_end))
 
-        start = np.array((y_start, x_start))
+        start = np.array((x_start, y_start))
+        size = np.array((height, width)) # width and height wrong?
+
         extracted = im_array[y_start:y_end,x_start:x_end,:]
-        return start, extracted
+        return start, extracted, size
 
 
 def point_corr(kp1, des1, kp2, des2, img1, img2): #get corresponding points between two sift point lists
@@ -203,7 +205,7 @@ def point_corr(kp1, des1, kp2, des2, img1, img2): #get corresponding points betw
         print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
         matchesMask = None
 
-    show_plot = False
+    show_plot = True
 
     if show_plot:
         draw_params = dict(matchColor = (0,255,0), # draw matches in green color
@@ -229,8 +231,11 @@ def main():
 
     image = cv.imread('airport/img_20.jpg')
 
-    net.get_bbs(image)
-    start, extracted = net.bb_params()
+    extracted = grab_based(image)
+
+
+    #net.get_bbs(image)
+    #start, extracted, _ = net.bb_params()
 
     img = cv.cvtColor(extracted,cv.COLOR_BGR2GRAY)
 
@@ -240,8 +245,8 @@ def main():
 
     N = src.shape[0]
 
-    des = np.reshape(des,(N,2))
-    src = np.reshape(src,(N,2))
+    des = np.reshape(des,(N,2)) # image points
+    src = np.reshape(src,(N,2)) # object points
 
 
     # turn source image points into 3D pose
@@ -252,15 +257,49 @@ def main():
 
     p_size = 0.123/hp # pixel size in meters
 
+    #print()
+
     src = src-np.array([x0,y0]) # move coordinate system into middle of sign
     src = src*p_size # turn image coordinates into meters
     src = np.concatenate((src, np.zeros((N,1))),axis=1) # add 0 in Z direction
 
 
     # bring back original image coordinates from bounding box points
-    des = des + start
+    #des = des + start
 
-    
+    mtx = np.load('mtx.npy')
+    dist = np.load('dist.npy')
+
+    succ, rvec, tvec = cv.solvePnP(src, des, mtx, dist)
+
+
+    R, _ = cv.Rodrigues(rvec)
+
+    #print(tvec)
+    T = np.concatenate((R,tvec),axis = 1)
+
+    temp = np.array([0,0,0,1])
+    temp = np.reshape(temp,(1,4))
+
+    T = np.concatenate((T,temp), axis = 0)
+
+    T_inv = np.linalg.inv(T)
+    print(T_inv)
+
+    #print(T_inv[0:3,0:3])
+    #print(T_inv[0:3,3])
+    rvec2, _ = cv.Rodrigues(T_inv[0:3,0:3])
+    tvec2 = T_inv[0:3,3]
+
+
+
+    cv.aruco.drawAxis(image, mtx, dist, rvec, tvec, 0.2)
+
+    cv.imshow('overlayed', image)
+
+    if cv.waitKey(0) & 0xff == 27:
+        cv.destroyAllWindows()
+
 
 
 
@@ -282,6 +321,7 @@ def preprocess_airport():
     #cv.imshow('Blurred', np.hstack((airport,blurred)))
 
     img = cv.cvtColor(blurred,cv.COLOR_BGR2GRAY)
+
     return img
 
     #cv.imshow('gray',img)
@@ -291,7 +331,54 @@ def preprocess_airport():
     #    cv.destroyAllWindows()
 
 
+def grab_based(img):
+    #img = cv.imread('airport/img_21.jpg')
 
+
+    net = network()
+    net.get_bbs(img)
+    start, extracted, size = net.bb_params()
+
+
+    mask = np.zeros(img.shape[:2],np.uint8)
+
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+
+    rect = (start[0],start[1],size[1],size[0])
+    cv.grabCut(img,mask,rect,bgdModel,fgdModel,10,cv.GC_INIT_WITH_RECT)
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+
+
+    img = img*mask2[:,:,np.newaxis]
+
+    return img
+
+
+
+
+    #plt.imshow(img),plt.colorbar(),plt.show()
+
+def harris(img):
+    gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    # find Harris corners
+    gray = np.float32(gray)
+    dst = cv.cornerHarris(gray,2,3,0.04)
+    dst = cv.dilate(dst,None)
+    ret, dst = cv.threshold(dst,0.01*dst.max(),255,0)
+    dst = np.uint8(dst)
+    # find centroids
+    ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
+    # define the criteria to stop and refine the corners
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+    # Now draw them
+    res = np.hstack((centroids,corners))
+    res = np.int0(res)
+    img[res[:,1],res[:,0]]=[0,0,255]
+    img[res[:,3],res[:,2]] = [0,255,0]
+
+    return img
 
 
 if __name__ == '__main__':
