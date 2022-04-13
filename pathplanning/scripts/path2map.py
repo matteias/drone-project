d52cedf11270c5967b7df20119111e0869dace93
n7729697 <n7729697@gmail.com>
2022-04-13 15:12:03 +0200

    normalized pathplanning

--- pathplanning/scripts/path2map.py
+++ pathplanning/scripts/path2map.py
@@ -7,55 +7,46 @@ from a_star import *
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
 
-path = "/home/maciejw/dd2419_ws/src/course_packages/dd2419_resources/worlds_json/saal1.world.json"
+# path_world = "/home/maciejw/dd2419_ws/src/course_packages/dd2419_resources/worlds_json/saal3.world.json"
 
-def find_path(stx,sty,gox,goy):
+def find_path(start, end, path_world):
     '''
-    stx,sty: start point on map
-    gox,goy: goal point on map
+    start: start point on map
+    end: goal point on map
     px, py: path position
     '''
-    grid_size = 2.0  # [m]
-    robot_radius = 1.0  # [m]
+    grid_size = 1.5  # [m]
+    robot_radius = 0.5  # [m]
     
-    mapp = Mapping(path, 0.05, 2)
+    mapp = Mapping(path_world, 0.1, 2)
     matrx = mapp.matrix
-    range_of_map = matrx.shape
-    horizonal = range_of_map[0]
-    vertical = range_of_map[1]
+    # range_of_map = matrx.shape
+    # horizonal = range_of_map[0]
+    # vertical = range_of_map[1]
     # set obstable positions
     matrx_indx = np.nonzero(matrx == 1) # represent the walls
     oy = matrx_indx[0].tolist()
     ox = matrx_indx[1].tolist()
 
-    # path planning
-    # start and goal position on mapping
-    sx = (stx/mapp.step) + mapp.x_conv   # [m]
-    sy = (sty/mapp.step) + mapp.y_conv  # [m]
-    gx = (gox/mapp.step) + mapp.x_conv  # [m]
-    gy = (goy/mapp.step) + mapp.y_conv # [m]
+    # start and goal position
+    sx = (start[0]/mapp.step) - mapp.x_conv # [matrix]
+    sy = (start[1]/mapp.step) - mapp.y_conv  # [matrix]
+    gx = (end[0]/mapp.step) - mapp.x_conv # [matrix]
+    gy = (end[1]/mapp.step) - mapp.y_conv # [matrix]
+
+    print("startpoint and endpoint:", sx,sy,gx,gy)
     a_star = AStarPlanner(ox, oy, grid_size, robot_radius)
     rx, ry = a_star.planning(sx, sy, gx, gy)
     rx.reverse()
     ry.reverse()
-    # path to follow
-    px,py= [],[]
-    px.append(stx)
-    py.append(sty)
-    for i in rx:
-        temp=(i-mapp.x_conv)*mapp.step
-        px.append(temp)
-    for i in ry:
-        temp=(i-mapp.y_conv)*mapp.step
-        py.append(temp)
-    px.append(gox)
-    py.append(goy)
 
+    # path to follow
+    px,py=path_points(mapp,start,end,rx,ry)
     view_path(px,py)
     
     return px,py
 
-def view_path(pathx,pathy):
+def view_path(pathx, pathy):
     '''
     Publish the ROS path msgs containing the way point
     '''
@@ -70,7 +61,7 @@ def view_path(pathx,pathy):
         pose = PoseStamped()
         pose.pose.position.x = pointx
         pose.pose.position.y = pointy
-        pose.pose.position.z = 0.4
+        pose.pose.position.z = 0.3
         quaternion = quaternion_from_euler(0, 0, 0)
         pose.pose.orientation.x = quaternion[0]
         pose.pose.orientation.y = quaternion[1]
@@ -85,13 +76,11 @@ rospy.init_node('path_planner', anonymous=True)
 rate = rospy.Rate(10)
 
 if __name__ == '__main__':
-    stx=0.5
-    sty=2.5
-    gox=0.5
-    goy=0.5
+    start = [0.3, 0.15]
+    end = [1.1, 0.15]
     while not rospy.is_shutdown():
         try:
-            px,py = find_path(stx,sty,gox,goy)
+            px,py = find_path(start, end)
         except rospy.ROSInterruptException:
             pass
         rate.sleep()
